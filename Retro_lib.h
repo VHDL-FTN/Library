c17c90ef22b2591f78aa8398434257270dea3314
StefanSredojevic <stefan.sredojevic95@gmail.com>
2017-04-16 14:06:26 +0200

    Create Retro_lib.h

---
+++ Retro_lib.h
@@ -0,0 +1,18 @@
+#ifndef SRC_RETRO_LIB_H_
+#define SRC_RETRO_LIB_H_
+#include "xparameters.h"
+#include "xil_io.h"
+#define offset 4
+// Base add -> XPAR_GRAPH_IP_0_S00_AXI_BASEADDR
+
+void clear_slv_reg ()
+{
+	int i;
+	i=0;
+	for (i;i<100;i++)
+	{
+		Xil_Out32 (XPAR_GRAPH_IP_0_S00_AXI_BASEADDR+offset*i,0x00000000);
+	}
+}
+
+#endif /* SRC_RETRO_LIB_H_ */
