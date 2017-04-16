1616c2de120443b5b2df784904fd499e420492f7
StefanSredojevic <stefan.sredojevic95@gmail.com>
2017-04-16 18:37:50 +0200

    Dodata nova funkcija za definisanje objekta

--- Retro_lib.h
+++ Retro_lib.h
@@ -14,4 +14,36 @@ void clear_slv_reg ()
 	}
 }
 
+unsigned int create_object (int object, int size, int horizontal, int vertical, int color)
+{
+	unsigned int string;
+	string = 0x00000000;
+
+	//adjusting object type in 32 bits (31 downto 26)
+	object &= 0x0000003F;
+	object <<= 26;
+	string |= object;
+
+	//adjusting size of object in 32 bits (25 downto 24)
+	size &= 0x00000003;
+	size <<= 24;
+	string |= size;
+
+	//adjusting horizontal position of object in 32 bits (23 downto 16)
+	horizontal &= 0x000000ff;
+	horizontal <<= 16;
+	string |= horizontal;
+
+	//adjusting vertical position of object in 32 bits (15 downto 9)
+	vertical &= 0x0000007f;
+	vertical <<= 9;
+	string |= vertical;
+
+	//adjusting color position of object in 32 bits (8 downto 0)
+	color &= 0x000001ff;
+	string |= color;
+
+	return string;
+}
+
 #endif /* SRC_RETRO_LIB_H_ */
