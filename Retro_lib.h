17560defaf115c3c2b929f065ee99d4cd2375323
StefanSredojevic <stefan.sredojevic95@gmail.com>
2017-04-26 18:53:53 +0200

    bla bla
    bla bla

--- Retro_lib.h
+++ Retro_lib.h
@@ -6,7 +6,6 @@
 #include "xil_printf.h"
 #include "xil_io.h"
 
-
 #include "object_define.h"
 
 #define BASEADDR XPAR_GRAPH_IP_0_S00_AXI_BASEADDR
@@ -15,19 +14,5 @@
 
 void clear_slv_reg ();
 unsigned int create_object (int, int, int, int, int);
-
-void draw_same_horizontal_objects (int, int, int, int, int, int, int);
-//
-//void getBall (int, int, int, int);
-//
-//int ball_hit_left_wall ();
-//int ball_hit_right_wall ();
-//int ball_hit_top_wall ();
-//int ball_hit_bottom_wall ();
-//
-//void ball_mooving_left_up (int size, int positionX, int positionY, int color);
-//void ball_mooving_roght_up (int size, int positionX, int positionY, int color);
-//void ball_mooving_left_down (int size, int positionX, int positionY, int color);
-//void ball_mooving_right_down (int size, int positionX, int positionY, int color);
 
 #endif /* SRC_RETRO_LIB_H_ */
